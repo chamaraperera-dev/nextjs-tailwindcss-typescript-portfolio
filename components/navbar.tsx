1c3cd14ba3aa02350f917e4b00f144becb8b935d
Chamara <chamaraperera@gmail.com>
2023-03-27 07:00:17 +1000

    Initial Commit

---
+++ components/navbar.tsx
@@ -0,0 +1,9 @@
+'use client'; //This is a react component, so we need to tell the compiler to use the client compiler
+
+import React from 'react';
+
+const navbar = () => {
+  return <div>navbar</div>;
+};
+
+export default navbar;
