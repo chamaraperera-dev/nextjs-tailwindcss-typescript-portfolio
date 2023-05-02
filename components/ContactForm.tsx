21c6bc25b3c248c18a7f1b2a840d0b0d91b94081
Chamara <chamaraperera@gmail.com>
2023-05-02 22:49:58 +1000

    change contact form width

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -71,7 +71,7 @@ const ContactForm = () => {
           className="flex align-center justify-center my-16
         "
         >
-          <form className="w-full max-w-lg " onSubmit={handleSubmit(onSubmit)}>
+          <form className="w-full max-w-5xl " onSubmit={handleSubmit(onSubmit)}>
             <div className="flex flex-wrap mx-3 mb-6">
               <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                 <label
